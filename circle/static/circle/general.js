4354f88c595f1af9629fdd04f98a416ddd2b8bf3
Nick Lowery <nicholas.lowery@gmail.com>
2021-02-03 10:37:50 -0800

    Add hide timer and animation on alert messages. Add top-margin to h1

--- circle/static/circle/general.js
+++ circle/static/circle/general.js
@@ -1,10 +1,16 @@
 function alert_message(message) {
-      // Display message in an alert
-      console.log("Message: " + message);
-      const alertTemplateDom = document.querySelector("#alert-template");
-      const alert = alertTemplateDom.cloneNode(true);
-      const alertText = alert.querySelector("#alert-text");
-      alertText.innerHTML = message;
-      document.querySelector("#alert-holder").appendChild(alert);
-      alert.style.display = "block";
+  // Display message in an alert
+  console.log("Message: " + message);
+  const alertTemplateDom = document.querySelector("#alert-template");
+  const alert = alertTemplateDom.cloneNode(true);
+  const alertText = alert.querySelector("#alert-text");
+  alertText.innerHTML = message;
+  document.querySelector("#alert-holder").appendChild(alert);
+  alert.style.display = "block";
+  window.setTimeout(function(){
+    alert.style.animationPlayState = 'running';
+    alert.addEventListener('animationend', function() {
+      alert.remove();
+    });
+  }, 3000);
 }
