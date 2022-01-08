3be5426a50f72a00ffe14423f05777df69b9cddd
Nick Lowery <nicholaslowery@gmail.com>
2022-01-07 18:54:37 -0800

    Actually get rid of old venv and update settings for Heroku

--- storycircles/settings.py
+++ storycircles/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lmllzpu&4wvtv_pr_@^k066o8-y@k_jrh(y51tr!h^wuk0r)o_'
+import os
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
